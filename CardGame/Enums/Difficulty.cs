8b63bb0d0388ce9e861495c0f96f7439423898b2
Ltechsystem <joachimlow02@gmail.com>
2026-05-15 23:13:51 +0200

    Initial commit

---
+++ CardGame/Enums/Difficulty.cs
@@ -0,0 +1,6 @@
+namespace CardGame.Enums;
+
+public enum Difficulty
+{
+    
+}
---
+++ CardGame/Enums/PlayingCardSuite.cs
@@ -0,0 +1,9 @@
+namespace CardGame.Enums;
+
+public enum PlayingCardSuite
+{
+    Hearts = 1,
+    Diamonds = 2,
+    Clubs = 3,
+    Spades = 4
+}
